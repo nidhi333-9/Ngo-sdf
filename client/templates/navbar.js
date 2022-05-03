bb8d718da0ab97ff381cdc37419a104892b3f3fb
nidhikumari <kumari9936nidhi@gmail.com>
2022-05-03 17:42:59 +0530

    third change

--- client/templates/navbar.js
+++ client/templates/navbar.js
@@ -21,3 +21,48 @@ function changeImg() {
 }
 
 window.onload = changeImg;
+
+
+
+const readMoreBtn = document.querySelector('.read-more-btn');
+
+const text = document.querySelector('.text');
+
+readMoreBtn.addEventListener('click', (e) => {
+    text.classList.toggle('show-more')
+})
+
+const readMoreBtn1 = document.querySelector('.read-more-btn-1');
+
+const text1 = document.querySelector('.text-1');
+
+readMoreBtn1.addEventListener('click', (e) => {
+    text1.classList.toggle('show-more')
+})
+
+const readMoreBtn2 = document.querySelector('.read-more-btn-2');
+
+const text2 = document.querySelector('.text-2');
+
+readMoreBtn2.addEventListener('click', (e) => {
+    text2.classList.toggle('show-more')
+})
+
+
+const readMoreBtn3 = document.querySelector('.read-more-btn-3');
+
+const text3 = document.querySelector('.text-3');
+
+readMoreBtn3.addEventListener('click', (e) => {
+    text3.classList.toggle('show-more')
+})
+
+
+const readMoreBtn4 = document.querySelector('.read-more-btn-4');
+
+const text4 = document.querySelector('.text-4');
+
+readMoreBtn4.addEventListener('click', (e) => {
+    text4.classList.toggle('show-more')
+})
+
